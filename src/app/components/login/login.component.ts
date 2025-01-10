77567cf98935ed6283f462a0f5d1e997471b33cd
Alejandro0052 <mariaeugenialopez456@gmail.com>
2025-01-09 23:16:49 -0500

    Acomodando ventana emergente despues de iniciar sesion y al colocar credenciales equivocadas

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -22,48 +22,49 @@ export class LoginComponent {
 			this.isLoading = true;
 
 			this.authService
-				.login(formulario.value.username, formulario.value.password)
-				.then((response) => {
-					this.showToast('Bienvenido, ' + formulario.value.username);
-					this.router.navigate(['/home']).then(() => {
-						window.location.reload();
-					});
-				})
+			.login(formulario.value.username, formulario.value.password)
+			.then((response) => {
+				
+				this.showToast('Inicio de sesión exitoso', 'success', 10000);
+				this.router.navigate(['/home']).then(() => {
+
+				});
+			})
 				.catch((error) => {
-					this.showToast(
-						error.response.data.non_field_errors,
-						'error',
-					);
+					this.showToast('Credenciales incorrectas', 'error', 10000);
 				})
 				.finally(() => {
 					this.isLoading = false;
 				});
 		} else {
 			console.log('Formulario inválido');
+			
 		}
 	}
 
 
-	showToast(message: string, type: 'success' | 'error' = 'success'): void {
+	showToast(message: string, type: 'success' | 'error' = 'success', duration: number = 10000): void {
+	
 		const toastElement = document.createElement('div');
-
-		
-		toastElement.className =
-			'fixed bottom-4 right-4 text-white p-4 rounded-lg shadow-md';
-
-		if (type === 'error') {
-			toastElement.classList.add('bg-red-500'); 
-		} else {
-			toastElement.classList.add('bg-green-500'); 
-		}
-
+		toastElement.style.position = 'fixed';
+		toastElement.style.bottom = '16px';
+		toastElement.style.right = '16px';
+		toastElement.style.backgroundColor = type === 'error' ? '#f87171' : '#34d399'; 
+		toastElement.style.color = 'white';
+		toastElement.style.padding = '16px';
+		toastElement.style.borderRadius = '8px';
+		toastElement.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
+		toastElement.style.opacity = '1'; 
+		toastElement.style.transition = 'opacity 1s ease';
+		toastElement.style.zIndex = '9999';
 		toastElement.innerText = message;
-
 		document.body.appendChild(toastElement);
 
-		
 		setTimeout(() => {
-			toastElement.remove();
-		}, 3000);
+			toastElement.style.opacity = '0';
+			setTimeout(() => {
+				toastElement.remove();
+			}, 500); 
+		}, duration);
 	}
 }
